0db1bb12ea8c31fac9067014a4641c3ecda1a3de
gitlost <burmartke@gmail.com>
2018-01-08 21:38:36 +0000

    Version 1.0.1.

--- command.php
+++ command.php
@@ -10,6 +10,6 @@ if ( ! defined( 'WP_CLI' ) ) {
  * @when before_wp_load
  */
 $test_version_composer_json_different = function() {
-	WP_CLI::success( 'Test version composer.json different version 1.0.0.' );
+	WP_CLI::success( 'Test version composer.json different version 1.0.1.' );
 };
 WP_CLI::add_command( 'test-version-composer-json-different', $test_version_composer_json_different );
